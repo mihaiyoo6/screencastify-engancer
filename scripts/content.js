957a7312ed71d3190ea2f7a8abeab0d915867202
MihaiMijea <mihai.mijea.dev@gmail.com>
2023-03-11 12:51:46 +0100

    add initial

---
+++ scripts/content.js
@@ -0,0 +1,63 @@
+const updatePosition = (s = "100px", n = 20) =>
+  `${parseInt(s) + parseInt(n)}px`;
+
+const updateCamera = (root, newWidth = 200) => {
+  if (!root) return true;
+  const container = root.shadowRoot;
+  const cameraContainer = container.querySelector("cf-resizeable");
+  if (!cameraContainer) return true;
+  const leftPosition = cameraContainer.style.left;
+  const oldWidth = parseInt(cameraContainer.style.width);
+  const iframe = container.querySelector("iframe");
+  iframe.style.scale = 1.5;
+  cameraContainer.style.borderRadius = "50%";
+  cameraContainer.style.width = `${newWidth}px`;
+  cameraContainer.style.left = updatePosition(
+    leftPosition,
+    oldWidth - newWidth
+  );
+};
+
+async function run() {
+  const observerConfig = {
+    attributes: true,
+    attributeFilter: ["style"],
+  };
+  const observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+      if (mutation.attributeName === "style") {
+        updateCamera(document.querySelector("castify-draw-container"));
+      }
+    });
+  });
+
+  const bodyObserver = new MutationObserver(function (mutations_list) {
+    mutations_list.forEach(function (mutation) {
+      mutation.addedNodes.forEach(function (added_node) {
+        if (added_node.nodeName === "CASTIFY-DRAW-CONTAINER") {
+          const root = document.querySelector("castify-draw-container");
+          const cameraContainer =
+            root.shadowRoot.querySelector("cf-resizeable");
+          updateCamera(root);
+          root.shadowRoot.querySelector(".toolbox-wrapper").style.display =
+            "none";
+          observer.observe(cameraContainer, observerConfig);
+          // bodyObserver.disconnect();
+        }
+      });
+    });
+  });
+
+  bodyObserver.observe(document.body, { subtree: false, childList: true });
+}
+
+window.addEventListener(
+  "load",
+  function load(e) {
+    window.removeEventListener("load", load, false);
+    this.setTimeout(() => {
+      run();
+    }, 3000);
+  },
+  false
+);
